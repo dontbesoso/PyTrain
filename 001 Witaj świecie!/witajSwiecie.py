8d7c8bff18f5807b61edd9f53f7668d565a16750
dontbesoso <dontbesoso@gmail.com>
2022-09-29 22:25:34 +0200

    Create witajSwiecie.py

---
+++ 001 Witaj świecie!/witajSwiecie.py
@@ -0,0 +1,12 @@
+"""
+Witaj w pierwszym programie w Pythonie!
+Właśnie czytasz komentarz. To taka notatka, którą program pomija.
+W komentarzu możesz napisać co chcesz. Nie wpłynie to na program.
+
+Ten komentarz jest umieszczony w potrójnym znaku ". Zaczyna się nim i kończy.
+No i pewno widzisz go w innym kolorze niż reszta programu. Widzisz?
+
+"""
+
+print("Witaj świecie!")
+
